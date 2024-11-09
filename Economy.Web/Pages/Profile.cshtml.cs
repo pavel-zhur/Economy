3d9d0e29e98ec93691f93749dcd6570309bb48ac
Pavel Zhur <completely.real@gmail.com>
2024-11-09 07:25:22 +0000

    profile management

---
+++ Economy.Web/Pages/Profile.cshtml.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.Google;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Economy.Web.Pages
+{
+    public class ProfileModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+
+        public IActionResult OnPostLogin()
+        {
+            var redirectUrl = Url.Page("/Index");
+            var properties = new AuthenticationProperties { RedirectUri = redirectUrl };
+            return Challenge(properties, GoogleDefaults.AuthenticationScheme);
+        }
+
+        public IActionResult OnPostLogout()
+        {
+            var redirectUrl = Url.Page("/Index");
+            var properties = new AuthenticationProperties { RedirectUri = redirectUrl };
+            return SignOut(properties, CookieAuthenticationDefaults.AuthenticationScheme);
+        }
+    }
+}
