7fb05097010886acf60e8878a11a420c9f0a6972
Pavel Zhur <completely.real@gmail.com>
2024-11-09 11:58:31 +0000

    basics page

---
+++ Economy.Web/Pages/Basics.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Economy.Web.Pages
+{
+    public class BasicsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
