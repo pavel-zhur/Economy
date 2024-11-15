6f647dd1638f80a1da8609b312937cc4bf3085a6
Pavel Zhur <completely.real@gmail.com>
2024-11-15 16:17:50 +0000

    reconnecting, authenticating

--- Economy.Web/Hubs/ChatHub.cs
+++ Economy.Web/Hubs/ChatHub.cs
@@ -2,11 +2,23 @@ using Microsoft.AspNetCore.SignalR;
 
 namespace Economy.Web.Hubs;
 
-public class ChatHub : Hub
+public class ChatHub(ILogger<ChatHub> logger) : Hub
 {
+    public override async Task OnConnectedAsync()
+    {
+        await base.OnConnectedAsync();
+        logger.LogInformation($"User {Context.UserIdentifier} connected");
+    }
+
     public async Task SendMessage(string user, string message)
     {
-        await Clients.All.SendAsync("ReceiveMessage", user, message);
+        if (user == "abort")
+        {
+            Context.Abort();
+            return;
+        }
+
+        await Clients.All.SendAsync("ReceiveMessage", $"user {Context.UserIdentifier} {Context.User?.Identity?.Name}", message);
     }
 
     public async Task SendAudio(string user, byte[] audioData)
