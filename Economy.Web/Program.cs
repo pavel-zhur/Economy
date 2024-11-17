71549650a518e209d560777b1a655f474ceb03ab
Pavel Zhur <completely.real@gmail.com>
2024-11-17 18:47:51 +0000

    ca

--- Economy.Web/Program.cs
+++ Economy.Web/Program.cs
@@ -7,9 +7,6 @@ using System.Globalization;
 using Economy.UserStorage;
 using Economy.Web.Hubs;
 using Economy.Web.Tools;
-using MessagePack;
-using MessagePack.Formatters;
-using MessagePack.Resolvers;
 
 var builder = WebApplication.CreateBuilder(args);
 
